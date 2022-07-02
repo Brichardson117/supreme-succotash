6d97a9b2e330295659c730a1b2ef3a775dc857e3
Brittney <brittneyrichardson8@gmail.com>
2022-07-01 20:08:57 -0500

    added service-worker, manifest.json, and idb files for offline use

---
+++ public/js/idb.js
@@ -0,0 +1,30 @@
+let db;
+
+const request = indexedDB.open("budget_tracker", 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+
+  db.createObjectStore("new_transaction", { autoIncrement: true });
+};
+
+// upon a successful
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+  }
+};
+
+request.onerror = function (event) {
+ 
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  const transactonObjectStore = transaction.objectStore("new_transaction");
+
+  transactonObjectStore.add(record);
+}
