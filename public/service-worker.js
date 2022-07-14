9d3fd5ef9504139ef5b407926a4646e77b5d9301
Brittney <brittneyrichardson8@gmail.com>
2022-07-14 18:55:45 -0500

    repair

--- public/service-worker.js
+++ public/service-worker.js
@@ -45,19 +45,42 @@ self.addEventListener("activate", function (e) {
   );
 });
 
-self.addEventListener("fetch", function (e) {
-  console.log("fetch request : " + e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        // if cache is available, respond with cache
-        console.log("responding with cache : " + e.request.url);
-        return request;
-      } else {
-        // if there are no cache, try fetching request
-        console.log("file is not cached, fetching : " + e.request.url);
-        return fetch(e.request);
-      }
+
+
+self.addEventListener("fetch", function(event) {
+
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+           
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+           
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          
+          return caches.match("/");
+        }
+      });
     })
   );
 });
