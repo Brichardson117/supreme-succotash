59bd4008a69628d1d9ea4c02491ce2e98a1eb945
Brittney <brittneyrichardson8@gmail.com>
2022-07-14 18:48:47 -0500

    fix

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,16 @@
 const FILES_TO_CACHE = [
-  "./public/index.html",
-  "./public/js/index.js",
-  "./public/icons/icon-72x72.png",
-  "./public/icons/icon-96x96.png",
-  "./public/icons/icon-128x128.png",
-  "./public/icons/icon-152x152.png",
-  "./public/icons/icon-192x192.png",
-  "./public/icons/icon-384x384.png",
-  "./public/icons/icon-512x512.png",
-  "./public/css/styles.css",
+  "./index.html",
+  "./js/index.js",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+  "./css/styles.css",
+  "./manifest.json",
+  "./js/idb.js"
 ];
 const APP_PREFIX = "BudgetTracker-";
 const VERSION = "version_01";
